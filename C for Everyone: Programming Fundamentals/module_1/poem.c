de44c255eff1e3d64cb6697570f23eb226b56655
edserranoc <edison.serrano@cimat.mx>
2025-04-18 20:20:24 -0600

    Update module 1

---
+++ C for Everyone: Programming Fundamentals/module_1/poem.c
@@ -0,0 +1,7 @@
+# include <stdio.h>
+
+int main()
+{
+    printf("A thing of beauty is a joy for ever:\nIts loveliness increases; it will never\nPass into nothingness; but still will keep\nA bower quiet for us, and a sleep\nFull of sweet dreams, and health, and quiet breathing.\n");
+    return 0;
+}
